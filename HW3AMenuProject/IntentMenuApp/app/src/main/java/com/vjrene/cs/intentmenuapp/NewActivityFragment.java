227e20c6ceb8ea5b07e24a8057ff0db8a8fe785f
Vanessa Rene <vjrene@gmail.com>
2016-06-16 13:42:25 -0400

    HW3A Menu Proj Min SDK 23

---
+++ HW3AMenuProject/IntentMenuApp/app/src/main/java/com/vjrene/cs/intentmenuapp/NewActivityFragment.java
@@ -0,0 +1,22 @@
+package com.vjrene.cs.intentmenuapp;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class NewActivityFragment extends Fragment {
+
+    public NewActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_new, container, false);
+    }
+}
---
+++ HW3AMenuProject/IntentMenuApp/app/src/main/java/com/vjrene/cs/intentmenuapp/SetActivity.java
@@ -0,0 +1,36 @@
+package com.vjrene.cs.intentmenuapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class SetActivity extends AppCompatActivity {
+
+    public void backUp(View view)
+    {
+        final Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
