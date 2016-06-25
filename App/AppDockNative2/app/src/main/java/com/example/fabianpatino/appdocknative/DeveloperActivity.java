0cf342c0d4e49222d85b6bced62b46314ba0295e
Fabian Patino <frpatino@gmail.com>
2016-06-25 14:07:02 -0400

    created basic java with corresponging xmls

---
+++ App/AppDockNative2/app/src/main/java/com/example/fabianpatino/appdocknative/DeveloperActivity.java
@@ -0,0 +1,15 @@
+package com.example.fabianpatino.appdocknative;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by fabianpatino on 6/25/16.
+ */
+public class DeveloperActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dev_page);
+    }
+}
--- App/AppDockNative2/app/src/main/java/com/example/fabianpatino/appdocknative/MainActivity.java
+++ App/AppDockNative2/app/src/main/java/com/example/fabianpatino/appdocknative/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.fabianpatino.appdocknative;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity { //This is the home page
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
